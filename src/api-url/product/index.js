96cc049d521b819f15190990f82d7b0a70b01c40
xzneozx96 <tuananhngyen218@gmail.com>
2022-06-15 17:43:42 +0700

    setup api-url and assets folder

---
+++ src/api-url/product/index.js
@@ -0,0 +1,6 @@
+import { BASE_API_URL } from "@utils/constants";
+
+export const API_PRODUCT = {
+  PRODUCT_LIST: `${BASE_API_URL}/product/list`,
+  PRODUCT_DETAIL: `${BASE_API_URL}/product/detail`,
+};
